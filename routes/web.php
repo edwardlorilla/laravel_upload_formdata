366d5c0a73521a69971d3b6683071d11452e5b14
Edward Lance Lorilla <edwardlorilla1986@gmail.com>
2016-12-30 01:37:40 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+
+use Illuminate\Support\Facades\Input;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('submit', function () {
+    foreach (Input::file('image') as $image) {
+        $imagename = time() . $image->getClientOriginalName();
+        $uploadFile = $image->move('public/uploads', $imagename);
+        if ($uploadFile) {
+            $uploadedImage[] = $imagename;
+        }
+    }
+    return response()->json(['success' => true, 'message' => 'images uploaded']);
+
+})->name('submit');
